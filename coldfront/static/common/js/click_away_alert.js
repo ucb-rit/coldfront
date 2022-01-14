f0d604db7c8fdea4332a3515763f8ab8a785ad43
jofeinstein <jofeinstein@gmail.com>
2022-01-14 13:09:32 -0500

    added onbeforeunload

--- coldfront/static/common/js/click_away_alert.js
+++ coldfront/static/common/js/click_away_alert.js
@@ -1,7 +1,10 @@
+var clickBool = false;
+
 $('body').on('click', function(e) {
     var target, href;
     target = $(e.target);
     if (e.target.tagName === 'A' && e.target.id !== '') {
+        clickBool = true;
         e.preventDefault();
 
         if (window.confirm("Are you sure you want to leave? You will lose progress on the current form.")) {
@@ -14,3 +17,9 @@ $('body').on('click', function(e) {
         }
     }
 });
+
+window.onbeforeunload = function() {
+    if (!clickBool) {
+        return true;
+    }
+};
