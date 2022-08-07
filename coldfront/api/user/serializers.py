048238d67eed33aa5df782973cad58e429e4cb6e
Joseph Feinstein <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>
2022-08-07 12:52:17 -0400

    adding ClusterAccountDeactivationRequestSerializer

--- coldfront/api/user/serializers.py
+++ coldfront/api/user/serializers.py
@@ -1,3 +1,7 @@
+from coldfront.core.allocation.models import \
+    ClusterAccountDeactivationRequestReasonChoice, \
+    ClusterAccountDeactivationRequestStatusChoice, \
+    ClusterAccountDeactivationRequest
 from coldfront.core.user.models import IdentityLinkingRequest
 from coldfront.core.user.models import IdentityLinkingRequestStatusChoice
 from django.contrib.auth.models import User
@@ -28,3 +32,40 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'first_name', 'last_name', 'email')
+
+
+class ClusterAccountDeactivationRequestSerializer(serializers.ModelSerializer):
+    """A serializer for the ClusterAccountDeactivationRequest model."""
+    user = UserSerializer(read_only=True, required=False)
+
+    status = serializers.SlugRelatedField(
+        slug_field='name',
+        queryset=ClusterAccountDeactivationRequestStatusChoice.objects.all())
+
+    reason = serializers.SlugRelatedField(
+        slug_field='name',
+        allow_null=True,
+        required=False,
+        read_only=True
+    )
+
+    justification = serializers.CharField(allow_null=True,
+                                          required=False)
+
+    class Meta:
+        model = ClusterAccountDeactivationRequest
+        fields = (
+            'id', 'user', 'status', 'reason', 'justification')
+        extra_kwargs = {
+            'id': {'read_only': True}
+        }
+
+    def validate(self, data):
+        # If the status is being changed to 'Cancelled', ensure that a
+        # justification are given.
+        if 'status' in data and data['status'].name == 'Cancelled':
+            if not isinstance(data.get('justification', None), str):
+                message = 'No justification is given.'
+                raise serializers.ValidationError(message)
+
+        return data
