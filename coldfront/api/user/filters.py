cdf80308238de50b0a8e8e8ecf4a77926f13c7d4
Joseph Feinstein <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>
2022-08-07 12:49:54 -0400

    added ClusterAccountDeactivationRequestFilter

--- coldfront/api/user/filters.py
+++ coldfront/api/user/filters.py
@@ -1,3 +1,7 @@
+from coldfront.core.allocation.models import \
+    ClusterAccountDeactivationRequestStatusChoice, \
+    ClusterAccountDeactivationRequest, \
+    ClusterAccountDeactivationRequestReasonChoice
 from coldfront.core.user.models import IdentityLinkingRequest
 from coldfront.core.user.models import IdentityLinkingRequestStatusChoice
 import django_filters.filters
@@ -13,3 +17,19 @@ class IdentityLinkingRequestFilter(django_filters.FilterSet):
     class Meta:
         model = IdentityLinkingRequest
         fields = ('status',)
+
+
+class ClusterAccountDeactivationRequestFilter(django_filters.FilterSet):
+    """A FilterSet for the ClusterAccountDeactivationRequest model."""
+
+    status = django_filters.filters.ModelMultipleChoiceFilter(
+        field_name='status__name', to_field_name='name',
+        queryset=ClusterAccountDeactivationRequestStatusChoice.objects.all())
+
+    reason = django_filters.filters.ModelMultipleChoiceFilter(
+        field_name='reason__name', to_field_name='name',
+        queryset=ClusterAccountDeactivationRequestReasonChoice.objects.all())
+
+    class Meta:
+        model = ClusterAccountDeactivationRequest
+        fields = ('status', 'reason')
