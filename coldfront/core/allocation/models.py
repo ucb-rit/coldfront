7ec7996a3e7db69975c56b1e2710c2c6e3c22de0
Joseph Feinstein <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>
2022-08-12 12:09:47 -0400

    added user_data_deletion to deletion request state schema

--- coldfront/core/allocation/models.py
+++ coldfront/core/allocation/models.py
@@ -689,6 +689,10 @@ def account_deletion_request_state_schema():
             'status': 'Pending',
             'timestamp': ''
         },
+        'user_data_deletion': {
+            'status': 'Pending',
+            'timestamp': ''
+        },
         'data_deletion': {
             'status': 'Pending',
             'timestamp': ''
