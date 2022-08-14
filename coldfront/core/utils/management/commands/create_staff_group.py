2beb9246ffcbb3c4621b3759edfcfd65f54135e0
Joseph Feinstein <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>
2022-08-14 19:04:10 -0400

    added view account deletion requests to staff group

--- coldfront/core/utils/management/commands/create_staff_group.py
+++ coldfront/core/utils/management/commands/create_staff_group.py
@@ -25,7 +25,8 @@ class Command(BaseCommand):
             'view_job',
             'view_securediradduserrequest',
             'view_securedirremoveuserrequest',
-            'view_securedirrequest'
+            'view_securedirrequest',
+            'view_accountdeletionrequest'
         ]
 
         for perm_codename in perm_codename_lst:
