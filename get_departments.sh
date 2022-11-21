8a9f405f35de59d7ab9597f227479e146ab58b78
Hamza Kundi <hamza.kkundi@gmail.com>
2022-11-21 12:03:59 -0800

    remove redundant code; use ldap for getting OU information

--- get_departments.sh
+++ get_departments.sh
@@ -1,16 +1,5 @@
-# Search through all users as so:
-# ldapsearch -LLL -H ldaps://ldap.berkeley.edu \
-#            -x -D "ou=people,dc=berkeley,dc=edu" \
-#                  "(&(objectClass=person)(mail=sahai@eecs.berkeley.edu))"
-#                  berkeleyEduPrimaryDeptUnit berkeleyEduUnitHRDeptName
-# If an email is not found, then search using name::w
-# ldapsearch -LLL -H ldaps://ldap.berkeley.edu \
-#            -x -D "ou=people,dc=berkeley,dc=edu" \
-#                  "(&(objectClass=person)(givenName=Anant)(sn=Sahai))" \
-#                  berkeleyEduPrimaryDeptUnit berkeleyEduUnitHRDeptName
-# Print out berkeleyEduPrimaryDeptUnit and berkeleyEduUnitHRDeptName
-
 shopt -s extglob
+echo "email,first_name,last_name,dept_unit,dept_unit_hierarchy,dept_unit_desc,dept_num,dept_num_hierarchy,dept_num_desc,dept_name"
 sudo -u postgres psql -d cf_brc_db -c \
     "SELECT ea.email, au.first_name, au.last_name \
     FROM project_projectuser AS pu INNER JOIN auth_user AS au ON \
@@ -24,51 +13,51 @@ sudo -u postgres psql -d cf_brc_db -c \
         dept_unit=""
         dept_name=""
         dept_num=""
-        for ou in "people" "advcon people" "guests" "expired people"; do
-            if [[ "$dept_unit" == "" ]]; then
-                while read line; do
-                    if [[ $(echo $line | grep berkeleyEduPrimaryDeptUnit | wc -l) -gt 0 ]]; then
-                        dept=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
-                    elif [[ $(echo $line | grep berkeleyEduUnitHRDeptName | wc -l) -gt 0 ]]; then
-                        dept_name=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
-                    elif [[ $(echo $line | grep departmentNumber | wc -l) -gt 0 ]]; then
-                        dept_num=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
-                    fi
-                done < <(ldapsearch -LLL -H ldaps://ldap.berkeley.edu \
-                        -x -D "ou=$ou,dc=berkeley,dc=edu" \
-                        "(&(objectClass=person)(mail=$email))" \
-                        berkeleyEduPrimaryDeptUnit berkeleyEduUnitHRDeptName \
-                        departmentNumber | grep -v '^$' | grep -v '^dn:')
-            fi
+        dept_unit_hierarchy=""
+        dept_unit_desc=""
+        dept_num_hierarchy=""
+        dept_num_desc=""
 
-            if [[ "$dept_unit" == "" ]]; then
-                while read line; do
-                    if [[ $(echo $line | grep berkeleyEduPrimaryDeptUnit | wc -l) -gt 0 ]]; then
-                        dept=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
-                    elif [[ $(echo $line | grep berkeleyEduUnitHRDeptName | wc -l) -gt 0 ]]; then
-                        dept_name=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
-                    elif [[ $(echo $line | grep departmentNumber | wc -l) -gt 0 ]]; then
-                        dept_num=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
-                    fi
-                done < <(ldapsearch -LLL -H ldaps://ldap.berkeley.edu \
-                        -x -D "ou=$ou,dc=berkeley,dc=edu" \
-                        "(&(objectClass=person)(givenName=$first_name)(sn=$last_name))" \
-                        berkeleyEduPrimaryDeptUnit berkeleyEduUnitHRDeptName \
-                        departmentNumber | grep -v '^$' | grep -v '^dn:')
+        for filter in "(mail=$email)" "(givenName=$first_name)(sn=$last_name)"; do
+            while read line; do
+                if [[ $(echo $line | grep berkeleyEduPrimaryDeptUnit | wc -l) -gt 0 ]]; then
+                    dept_unit=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
+                elif [[ $(echo $line | grep berkeleyEduUnitHRDeptName | wc -l) -gt 0 ]]; then
+                    dept_name=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
+                elif [[ $(echo $line | grep departmentNumber | wc -l) -gt 0 ]]; then
+                    dept_num=$(echo $line | cut -d' ' -f2- | xargs -0 echo)
+                fi
+            done < <(ldapsearch -LLL -H ldaps://ldap.berkeley.edu \
+                    -x -D "ou=,dc=berkeley,dc=edu" \
+                    "(&(objectClass=person)$filter)" \
+                    berkeleyEduPrimaryDeptUnit berkeleyEduUnitHRDeptName \
+                    departmentNumber | grep -v '^$' | grep -v '^dn:')
+            if [[ "$dept_unit" != "" ]]; then
+                break 1
             fi
         done
 
-        email="${email##*( )}"
-        email="${email%%*( )}"
-        first_name="${first_name##*( )}"
-        first_name="${first_name%%*( )}"
-        last_name="${last_name##*( )}"
-        last_name="${last_name%%*( )}"
-        dept_unit="${dept_unit##*( )}"
-        dept_unit="${dept_unit%%*( )}"
-        dept_name="${dept_name##*( )}"
-        dept_name="${dept_name%%*( )}"
-        dept_num="${dept_num##*( )}"
-        dept_num="${dept_num%%*( )}"
-        echo "$email,$first_name,$last_name,$dept,$dept_name,$dept_num"
+        if [[ "$dept_unit" != "" ]]; then
+            for type in "unit" "num"; do
+                while read line; do
+                    if [[ $(echo $line | grep berkeleyEduOrgUnitHierarchyString | wc -l) -gt 0 ]]; then
+                        eval "dept_${type}_hierarchy=\"$(echo $line | cut -d' ' -f2- | xargs -0 echo)\""
+                    elif [[ $(echo $line | grep description | wc -l) -gt 0 ]]; then
+                        eval "dept_${type}_desc=\"$(echo $line | cut -d' ' -f2- | xargs -0 echo)\""
+                    fi
+                done < <(ldapsearch -LLL -H ldaps://ldap.berkeley.edu \
+                        -x -D "ou=people,dc=berkeley,dc=edu" \
+                        "(&(objectClass=organizationalunit)(ou=$(eval echo "\$dept_${type}")))" \
+                        berkeleyEduOrgUnitHierarchyString description \
+                        | grep -v '^$' | grep -v '^dn:')
+            done
+        fi
+        
+        for var in "email" "first_name" "last_name" "dept_unit" "dept_name" \
+                   "dept_num" "dept_unit_hierarchy" "dept_unit_desc" \
+                   "dept_num_hierarchy" "dept_num_desc"; do
+            eval "$var=\"\${$var##*( )}\""
+            eval "$var=\"\${$var%%*( )}\""
+        done
+        echo "$email,$first_name,$last_name,$dept_unit,$dept_unit_hierarchy,$dept_unit_desc,$dept_num,$dept_num_hierarchy,$dept_num_desc,$dept_name"
     done
