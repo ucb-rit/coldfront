85aaf2c953b84642310537e821e605c85a42bb88
Matthew Li <matthew.elbert.li@gmail.com>
2023-04-19 11:23:25 -0700

    Adjust style in LDAP library

--- coldfront/core/department/utils/ldap.py
+++ coldfront/core/department/utils/ldap.py
@@ -9,15 +9,17 @@ LDAP_URL = 'ldap.berkeley.edu'
 PEOPLE_OU = 'ou=people,dc=berkeley,dc=edu'
 DEPARTMENT_OU = 'ou=org units,dc=berkeley,dc=edu'
 
+
 def ldap_search_user(email, first_name, last_name):
-    ''' Searches for a user in LDAP
+    """Search for a user in LDAP.
+
     Parameters:
         email (str): Email address of user
         first_name (str): First name of user
         last_name (str): Last name of user
     Returns:
         (ldap3.Entry) LDAP entry for user or None if not found
-    '''
+    """
     conn = Connection(LDAP_URL, auto_bind=True)
     if conn.search(PEOPLE_OU, f'(&(objectClass=person)(mail={email}))',
                                             attributes=['departmentNumber']):
@@ -28,13 +30,15 @@ def ldap_search_user(email, first_name, last_name):
         return conn.entries[0]
     return None
 
+
 def get_L4_code_from_department_code(code):
-    ''' Returns the L4 code for a department from a L4+ code
+    """Return the L4 code for a department from a L4+ code.
+
     Parameters:
         code (str): L4+ department code
     Returns:
         (str) Normalized L4 department code
-    '''
+    """
     conn = Connection(LDAP_URL, auto_bind=True)
     conn.search(DEPARTMENT_OU,
                 f'(&(objectClass=organizationalUnit)(ou={code}))',
@@ -42,28 +46,34 @@ def get_L4_code_from_department_code(code):
     hierarchy = conn.entries[0].berkeleyEduOrgUnitHierarchyString.value
     return hierarchy.split('-')[3]
 
+
 def get_department_name_from_code(code):
-    ''' Returns the name of a department from a code
+    """Return the name of a department from a code.
+
     Parameters:
        code (str): department code
     Returns:
         (str) Name of department
-    '''
+    """
     conn = Connection(LDAP_URL, auto_bind=True)
     conn.search(DEPARTMENT_OU,
                 f'(&(objectClass=organizationalUnit)(ou={code}))',
                 attributes=['description'])
     return conn.entries[0].description.value
 
+
 def fetch_and_set_user_departments(user, userprofile, dry_run=False):
-    ''' Fetches a user's departments from LDAP and sets them in the database
+    """Fetch a user's departments from LDAP and sets them in the
+    database.
+
     Parameters:
         user (User): Django User object
         userprofile (UserProfile): Coldfront UserProfile object
-        dry_run (bool): If True, don't actually make changes (default: False)
+        dry_run (bool): If True, don't actually make changes
+                        (default: False)
     Returns:
         None
-    '''
+    """
     if dry_run:
         logger.info = print
         department = Department()
@@ -74,7 +84,7 @@ def fetch_and_set_user_departments(user, userprofile, dry_run=False):
     ldap_departments = user_entry.departmentNumber.values if user_entry else []
     for department_code in ldap_departments:
         department_code = get_L4_code_from_department_code(department_code)
-        # if department doesn't exist, make it
+        # If a Department doesn't exist, create it.
         name = None if Department.objects.filter(code=department_code).exists() \
             else get_department_name_from_code(department_code)
         if dry_run:
@@ -89,7 +99,8 @@ def fetch_and_set_user_departments(user, userprofile, dry_run=False):
             logger.info(f'Created Department {department.pk}, '
                         f'{department.name} ({department.code})')
 
-        # Create UserDepartment association, updating is_authorative if needed
+        # Create a UserDepartment association, updating is_authoritative if
+        # needed.
         if not dry_run:
             userdepartment, created = UserDepartment.objects.update_or_create(
                                             userprofile=userprofile,
