6c64b07fcf92fa8903eb2d2baabf10ba182fa50e
tyfong-lbl <130508739+tyfong-lbl@users.noreply.github.com>
2024-04-04 15:27:48 -0700

    Add debugger call for form validity

--- coldfront/core/project/tests/test_forms/test_new_project_forms/test_savio_project_review_form.py
+++ coldfront/core/project/tests/test_forms/test_new_project_forms/test_savio_project_review_form.py
@@ -3,26 +3,32 @@ from coldfront.core.project.forms_.new_project_forms.approval_forms import Savio
 
 
 class SavioProjectReviewSetupFormTest(TestBase):
+    def setUp(self):
+        super().setUp()
+        self.requested_name =  f"""rc_{'a'*50}""",
+        self.computing_allowance =  'test_allowance'
+
     def test_form_with_pooling_true(self):
         """Test the form with a final_name length of 50 chars and pooling==True"""
         form_data = {
-            'pooling': True,
-            'project_pk': 1, #Just for testing
-            'requested_name': 'a' * 50,
-            'computing_allowance': 'test_allowance'
-        }
-        form = SavioProjectReviewSetupForm(**form_data)
+            'requested_name': self.requested_name,
+            'project_pk': 1,
+            'computing_allowance': self.computing_allowance,
+            'pooling':True,
+         }
+        form = SavioProjectReviewSetupForm(**form_data) 
         import pdb; pdb.set_trace()
-        self.assertTrue(form.is_valid())
+        if not form.is_valid():
+            print(form.errors) 
+        self.assertTrue(form.is_valid()), "Form should be valid when pooling is true and final name exceeds 15 characters "
 
     def test_form_with_pooling_false(self):
         """Test the form with a final_name length of 50 chars and pooling==False"""
         form_data = {
             'pooling': False,
+            'requested_name': self.requested_name,
             'project_pk': 1,
-            'requested_name': 'a' * 50,
             'computing_allowance': 'test_allowance',
-            'status': 'Pending'
         }
         form = SavioProjectReviewSetupForm(**form_data)
         self.assertFalse(form.is_valid(), """Form should not be valid as 
