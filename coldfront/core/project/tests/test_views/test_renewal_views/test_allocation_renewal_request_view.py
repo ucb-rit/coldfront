6bd057c4ffd22315c2a6119082c8c12197a2f0ca
Rae Xin <raexin@berkeley.edu>
2024-04-26 15:04:45 -0700

    Formatting for 'renewal_survey' big huge JSON

--- coldfront/core/project/tests/test_views/test_renewal_views/test_allocation_renewal_request_view.py
+++ coldfront/core/project/tests/test_views/test_renewal_views/test_allocation_renewal_request_view.py
@@ -56,7 +56,28 @@ class TestAllocationRenewalRequestView(TestBase):
 
         allocation_period = get_current_allowance_year_period()
 
-        renewal_survey = [{"which_brc_services_used":"srdc"},{"publications_supported_by_brc":"N/A"},{"grants_supported_by_brc":"N/A"},{"recruitment_or_retention_cases":"N/A"},{"classes_being_taught":"N/A"},{"brc_recommendation_rating":6},{"brc_recommendation_rating_reason":""},{"how_brc_helped_bootstrap_computational_methods":"N/A"},{"how_important_to_research_is_brc":2},{"do_you_use_mybrc":"no"},{"mybrc_comments":""},{"which_open_ondemand_apps_used":"jupyter_notebook"},{"which_open_ondemand_apps_used":"vscode_server"},{"brc_feedback":""},{"colleague_suggestions":""},{"indicate_topic_interests":"have_had_rdmp_event_or_consultation"},{"indicate_topic_interests":"want_to_learn_more_and_have_rdm_consult"},{"training_session_usefulness_of_computational_platforms_training":2},{"training_session_usefulness_of_basic_savio_cluster":4},{"training_session_other_topics_of_interest":""}]
+        renewal_survey = [
+            {"which_brc_services_used":"srdc"},
+            {"publications_supported_by_brc":"N/A"},
+            {"grants_supported_by_brc":"N/A"},
+            {"recruitment_or_retention_cases":"N/A"},
+            {"classes_being_taught":"N/A"},
+            {"brc_recommendation_rating":6},
+            {"brc_recommendation_rating_reason":""},
+            {"how_brc_helped_bootstrap_computational_methods":"N/A"},
+            {"how_important_to_research_is_brc":2},
+            {"do_you_use_mybrc":"no"},
+            {"mybrc_comments":""},
+            {"which_open_ondemand_apps_used":"jupyter_notebook"},
+            {"which_open_ondemand_apps_used":"vscode_server"},
+            {"brc_feedback":""},
+            {"colleague_suggestions":""},
+            {"indicate_topic_interests":"have_had_rdmp_event_or_consultation"},
+            {"indicate_topic_interests":"want_to_learn_more_and_have_rdm_consult"},
+            {"training_session_usefulness_of_computational_platforms_training":2},
+            {"training_session_usefulness_of_basic_savio_cluster":4},
+            {"training_session_other_topics_of_interest":""}
+                ]
         
         view_name = 'allocation_renewal_request_view'
         current_step_key = f'{view_name}-current_step'
