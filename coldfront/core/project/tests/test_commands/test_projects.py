7b59b95b461b6cccad44e32238e6386cad739ea6
Alastair Deng <adeng27@stanford.edu>
2024-08-06 13:39:51 -0700

    flesh out testing for project renewals

--- coldfront/core/project/tests/test_commands/test_projects.py
+++ coldfront/core/project/tests/test_commands/test_projects.py
@@ -5,16 +5,20 @@ from django.conf import settings
 from django.core.management import call_command
 from django.core.management import CommandError
 from django.db.models import Q
+from django.contrib.auth.models import User
 
 from coldfront.api.statistics.utils import create_project_allocation
 
 from coldfront.core.allocation.models import AllocationPeriod
+from coldfront.core.allocation.models import AllocationRenewalRequest
+from coldfront.core.allocation.models import AllocationRenewalRequestStatusChoice
 from coldfront.core.project.tests.test_commands.test_service_units_base import TestSUBase
 from coldfront.core.project.models import Project
 from coldfront.core.project.models import ProjectStatusChoice
 from coldfront.core.project.models import ProjectUser
 from coldfront.core.project.models import ProjectUserRoleChoice
 from coldfront.core.project.models import ProjectUserStatusChoice
+from coldfront.core.project.utils_.renewal_utils import get_current_allowance_year_period
 from coldfront.core.resource.models import Resource
 from coldfront.core.resource.utils_.allowance_utils.interface import ComputingAllowanceInterface
 from coldfront.core.utils.common import utc_now_offset_aware
@@ -130,8 +134,15 @@ class TestProjectsRenew(TestProjectsBase):
             Decimal(self.service_units_attribute.value),
             self.num_service_units)
 
+        request = AllocationRenewalRequest.objects.get(
+            requester=self.user,
+            pi=self.user,
+            pre_project=project
+        )
+        self.assertEqual(
+            AllocationRenewalRequestStatusChoice.objects.get(name="Complete"),
+            request.status)
         # TODO: Also test:
-        #  That a request gets created and it's in the "Complete" state
         #  That only a processing email is sent
 
     def test_validate_project(self):
@@ -146,28 +157,201 @@ class TestProjectsRenew(TestProjectsBase):
                 'invalid project name', self.allocation_period,
                 self.user.username, self.user.username)
         self.assertIn('A Project with name', str(cm.exception))
-        # TODO: Refactor the "not successful" checks into a block for reuse.
-        still_inactive_proj = Project.objects.get(name=self.project_name)
+        self.assertProjectInactive(self.project_name)
+        self.service_units_attribute.refresh_from_db()
         self.assertEqual(
-            still_inactive_proj.status,
-            ProjectStatusChoice.objects.get(name='Inactive'))
+            Decimal(self.service_units_attribute.value),
+            settings.ALLOCATION_MIN)
+    
+    def test_validate_computing_allowance(self):
+        """Test that computing allowance that cannot be renewed fail 
+        correctly (e.g., Recharge, Condo)."""
+        
+        non_renewable_resources = ['Condo Allocation', 'Recharge Allocation']
+        computing_allowance_interface = ComputingAllowanceInterface()
+        for resource_name in non_renewable_resources:
+            computing_allowance = Resource.objects.get(
+                name=resource_name)
+            project_name_prefix = computing_allowance_interface.code_from_name(
+                computing_allowance.name)
+            project_name = project_name_prefix + "testproject"
+            Project.objects.create(
+                name=project_name,
+                title=project_name,
+                status=ProjectStatusChoice.objects.get(name='Inactive'))
+            
+            with self.assertRaises(CommandError) as cm:
+                self.command.renew(
+                    project_name, self.allocation_period,
+                    self.user.username, self.user.username)
+            self.assertIn('is not renewable', str(cm.exception))
+            self.assertProjectInactive(project_name)
+            
+    def test_validate_requester(self):
+        """Test that requesters who do not exist, or are not an active
+        manager/PI fail correctly."""
+
+        project = Project.objects.get(name=self.project_name)
+        self.assertEqual(
+            project.status, ProjectStatusChoice.objects.get(name='Inactive'))
+
+        # Requester who is not manager/PI is invalid.
+        invalid_requester = User.objects.create(
+            email='invalid@gmail.com',
+            first_name='invalid',
+            last_name='invalid',
+            username='invalid_requester'
+        )
+        ProjectUser.objects.create(
+            project=project,
+            role=ProjectUserRoleChoice.objects.get(name="User"),
+            status=ProjectUserStatusChoice.objects.get(name='Active'),
+            user=invalid_requester)
+        
+        # Requester who is removed from project is invalid, even if Manager/PI
+        removed_requester = User.objects.create(
+            email='removed@gmail.com',
+            first_name='removed',
+            last_name='removed',
+            username='removed_requester'
+        )
+        ProjectUser.objects.create(
+            project=project,
+            role=ProjectUserRoleChoice.objects.get(name="Manager"),
+            status=ProjectUserStatusChoice.objects.get(name='Removed'),
+            user=removed_requester)
+        
+        invalid_usernames = [invalid_requester.username,
+                             removed_requester.username]
+        for invalid_username in invalid_usernames:
+            with self.assertRaises(CommandError) as cm:
+                self.command.renew(
+                    project.name, self.allocation_period,
+                    self.user.username, invalid_username)
+            self.assertIn(
+                f'Requester {invalid_username} is not an active member',
+                str(cm.exception))
+            self.assertProjectInactive(project.name)
+            self.service_units_attribute.refresh_from_db()
+            self.assertEqual(
+                Decimal(self.service_units_attribute.value),
+                settings.ALLOCATION_MIN)
+    
+    def test_validate_pi(self):
+        """Test that PI's who do not exist or are not active fail correctly."""
+
+        project = Project.objects.get(name=self.project_name)
+        self.assertEqual(
+            project.status, ProjectStatusChoice.objects.get(name='Inactive'))
+        
+        nonexistant_pi_username = 'nonexistant_pi_username'
+        with self.assertRaises(CommandError) as cm:
+                self.command.renew(
+                    project.name, self.allocation_period,
+                    nonexistant_pi_username, self.user.username)
+        self.assertIn(
+            f'User with username "{nonexistant_pi_username}" does not exist',
+            str(cm.exception))
+        self.assertProjectInactive(project.name)
+
+        # Active User (manager) who is on project but not PI
+        invalid_pi = User.objects.create(
+            email='manager@gmail.com',
+            first_name='manager',
+            last_name='manager',
+            username='invalid_pi'
+        )
+        ProjectUser.objects.create(
+            project=project,
+            role=ProjectUserRoleChoice.objects.get(name="Manager"),
+            status=ProjectUserStatusChoice.objects.get(name='Active'),
+            user=invalid_pi)
+        
+        # Removed PI
+        removed_pi = User.objects.create(
+            email='removedPI@gmail.com',
+            first_name='removed',
+            last_name='removed',
+            username='removed_pi'
+        )
+        ProjectUser.objects.create(
+            project=project,
+            role=ProjectUserRoleChoice.objects.get(
+                name="Principal Investigator"),
+            status=ProjectUserStatusChoice.objects.get(name='Removed'),
+            user=removed_pi)
+        invalid_pis = [invalid_pi.username, removed_pi.username]
+        for invalid_pi in invalid_pis:
+            with self.assertRaises(CommandError) as cm:
+                self.command.renew(
+                    project.name, self.allocation_period,
+                    invalid_pi, self.user.username)
+            self.assertIn(
+                f'{invalid_pi} is not an active PI',
+                str(cm.exception))
+            self.assertProjectInactive(project.name)
+            self.service_units_attribute.refresh_from_db()
+            self.assertEqual(
+                Decimal(self.service_units_attribute.value),
+                settings.ALLOCATION_MIN)
+
+    def test_validate_allocation_period(self):
+        """Test that AllocationPeriods which do not exist, are not valid for the
+        given computing allowance, or are not current fail correctly."""
+
+        project = Project.objects.get(name=self.project_name)
+        self.assertEqual(
+            project.status, ProjectStatusChoice.objects.get(name='Inactive'))
+        
+        nonexistant_alloc_period = "I don't exist!"
+        with self.assertRaises(CommandError) as cm:
+                self.command.renew(
+                    project.name, nonexistant_alloc_period,
+                    self.user.username, self.user.username)
+        self.assertIn(
+            f'AllocationPeriod "{nonexistant_alloc_period}" does not exist.',
+            str(cm.exception))
+        self.assertProjectInactive(project.name)
+
+        # Allowance Year allocation periods are not for ICA projects
+        cur_allowance_year = get_current_allowance_year_period()
+
+        with self.assertRaises(CommandError) as cm:
+            self.command.renew(
+                project.name, cur_allowance_year.name,
+                self.user.username, self.user.username)
+        self.assertIn(
+            f'"{cur_allowance_year.name}" is not a valid AllocationPeriod',
+            str(cm.exception))
+        self.assertProjectInactive(project.name)
+
+        # Non-current allocation period
+        past_alloc_period = AllocationPeriod.objects.filter(
+            Q(name__startswith='Fall') |
+            Q(name__startswith='Summer') |
+            Q(name__startswith='Spring'),
+            end_date__lt=utc_now_offset_aware())[0]
+        
+        with self.assertRaises(CommandError) as cm:
+            self.command.renew(
+                project.name, past_alloc_period,
+                self.user.username, self.user.username)
+        self.assertIn(
+            f'AllocationPeriod already ended',
+            str(cm.exception))
+        self.assertProjectInactive(project.name)
+
         self.service_units_attribute.refresh_from_db()
         self.assertEqual(
             Decimal(self.service_units_attribute.value),
             settings.ALLOCATION_MIN)
 
-    # TODO
-    #   test_validate_computing_allowance
-    #     ComputingAllowance isn't renewable (e.g., Recharge, Condo)
-    #   test_validate_requester
-    #     Requester validation: doesn't exist; isn't active manager/PI
-    #   test_validate_pi
-    #     PI validation: doesn't exist; isn't active PI
-    #   test_validate_allocation_period
-    #     AllocationPeriod doesn't exist
-    #     AllocationPeriod isn't valid for project's computing allowance
-    #     AllocationPeriod isn't current
-
+    def assertProjectInactive(self, project_name):
+        """ Test that a project has Inactive status. """
+        still_inactive_proj = Project.objects.get(name=project_name)
+        self.assertEqual(
+            still_inactive_proj.status,
+            ProjectStatusChoice.objects.get(name='Inactive'))
 
 class ProjectsCommand(object):
     """A wrapper class over the 'projects' management command."""
