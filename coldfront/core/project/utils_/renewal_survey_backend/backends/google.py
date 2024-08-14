df1657834010ca66c448fd03ee270fde4f800484
Alastair Deng <adeng27@stanford.edu>
2024-08-13 17:08:50 -0700

    refactoring

--- coldfront/core/project/utils_/renewal_survey_backend/backends/google.py
+++ coldfront/core/project/utils_/renewal_survey_backend/backends/google.py
@@ -1,7 +1,7 @@
 from coldfront.core.project.utils_.renewal_survey_backend.backends.base import BaseRenewalSurveyBackend
-from coldfront.core.project.utils_.renewal_utils import get_gspread_wks
-from coldfront.core.project.utils_.renewal_utils import get_renewal_survey
-from coldfront.core.project.utils_.renewal_utils import gsheet_column_to_index
+from coldfront.core.project.utils_.renewal_survey_utils import get_gspread_wks
+from coldfront.core.project.utils_.renewal_survey_utils import get_renewal_survey
+from coldfront.core.project.utils_.renewal_survey_utils import gsheet_column_to_index
 
 from django.core.exceptions import ValidationError
 
@@ -100,35 +100,3 @@ class GoogleRenewalSurveyBackend(BaseRenewalSurveyBackend):
                 PARAMETER_BASE_TWO + value
         return url
     
-
-def make_form_url(self, survey_data, dictionary):
-        """ This function creates the Google Form URL with appropriate
-         pre-filled parameters. """
-        BASE_URL_ONE = 'https://docs.google.com/forms/d/e/'
-        BASE_URL_TWO = '/viewform?usp=pp_url'
-        url = BASE_URL_ONE + survey_data['form_id'] + BASE_URL_TWO
-
-        PARAMETER_BASE_ONE = '&entry.'
-        PARAMETER_BASE_TWO = '='
-
-        question_ids_dict = survey_data['form_question_ids']
-        for parameter in question_ids_dict.keys():
-            value = ''
-            if parameter == 'allocation_period':
-                value = dictionary['allocation_period'].name
-            elif parameter == 'pi_name':
-                value = dictionary['PI'].user.first_name + '+' + \
-                    dictionary['PI'].user.last_name
-            elif parameter == 'pi_username':
-                value = dictionary['PI'].user.username
-            elif parameter == 'project_name':
-                value = self.project_obj.name
-            elif parameter == 'requester_name':
-                value = self.request.user.first_name + '+' + \
-                    self.request.user.last_name
-            elif parameter == 'requester_username':
-                value = self.request.user.username
-            value = value.replace(' ', '+')
-            url += PARAMETER_BASE_ONE + question_ids_dict[parameter] + \
-                PARAMETER_BASE_TWO + value
-        return url
--- coldfront/core/project/views_/renewal_views/request_views.py
+++ coldfront/core/project/views_/renewal_views/request_views.py
@@ -26,7 +26,7 @@ from coldfront.core.project.utils_.renewal_utils import has_non_denied_renewal_r
 from coldfront.core.project.utils_.renewal_utils import send_new_allocation_renewal_request_admin_notification_email
 from coldfront.core.project.utils_.renewal_utils import send_new_allocation_renewal_request_pi_notification_email
 from coldfront.core.project.utils_.renewal_utils import send_new_allocation_renewal_request_pooling_notification_email
-from coldfront.core.project.utils_.renewal_utils import get_renewal_survey
+from coldfront.core.project.utils_.renewal_survey_utils import get_renewal_survey
 from coldfront.core.project.utils_.renewal_survey_backend import get_survey_url
 from coldfront.core.resource.models import Resource
 from coldfront.core.resource.utils import get_primary_compute_resource
@@ -598,6 +598,7 @@ class AllocationRenewalRequestView(LoginRequiredMixin, UserPassesTestMixin,
     def make_form_url(self, survey_data, dictionary):
         """Get pre-filled survey link for user."""
         dictionary['requester'] = self.request.user
+        dictionary['project_name'] = dictionary['requested_project'].name
         return get_survey_url(survey_data, dictionary)
 
 class AllocationRenewalRequestUnderProjectView(LoginRequiredMixin,
