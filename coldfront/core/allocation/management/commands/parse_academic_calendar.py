d2c8ec0b01a624561d82d47428287c1bbbb0294b
Hamza Kundi <hamza.kkundi@gmail.com>
2025-06-30 15:39:18 -0500

    fixed json format

--- coldfront/core/allocation/management/commands/parse_academic_calendar.py
+++ coldfront/core/allocation/management/commands/parse_academic_calendar.py
@@ -4,6 +4,49 @@ import pypdf
 import json
 from django.core.management.base import BaseCommand
 
+EXAMPLE_JSON ='''[
+    {
+        "name": "Fall Semester 2025",
+        "start_date": "2025-08-20",
+        "end_date": "2025-12-19"
+    },
+    {
+        "name": "Spring Semester 2026",
+        "start_date": "2026-01-13",
+        "end_date": "2026-05-15"
+    },
+    {
+        "name": "Summer Sessions 2026 - Session A",
+        "start_date": "2026-05-26",
+        "end_date": "2026-07-02"
+    },
+    {
+        "name": "Summer Sessions 2026 - Session B",
+        "start_date": "2026-06-08",
+        "end_date": "2026-08-14"
+    },
+    {
+        "name": "Summer Sessions 2026 - Session C",
+        "start_date": "2026-06-22",
+        "end_date": "2026-08-14"
+    },
+    {
+        "name": "Summer Sessions 2026 - Session D",
+        "start_date": "2026-07-06",
+        "end_date": "2026-08-14"
+    },
+    {
+        "name": "Summer Sessions 2026 - Session E",
+        "start_date": "2026-07-27",
+        "end_date": "2026-08-14"
+    },
+    {
+        "name": "Summer Sessions 2026 - Session F",
+        "start_date": "2026-07-06",
+        "end_date": "2026-07-24"
+    }
+]'''
+
 class Command(BaseCommand):
     help = 'Parse the UCB academic calendar and output JSON entries.'
 
@@ -27,20 +70,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         year = options['calendar_year']
-        first_year = int(year.split('-')[0])
         api_key = options['api_key']
         api = options['api']
         pdf_url = f'https://registrar.berkeley.edu/wp-content/uploads/UCB_AcademicCalendar_{year}.pdf'
         
-        output_json_example = {
-            'academic_year': f'{year}',
-            'semesters': [
-                {'name': 'Fall', 'start_date': f'{first_year}-08-25', 'end_date': f'{first_year}-12-15'},
-                {'name': 'Spring', 'start_date': f'{first_year + 1}-01-10', 'end_date': f'{first_year + 1}-05-20'},
-                {'name': 'Summer', 'start_date': f'{first_year + 1}-06-01', 'end_date': f'{first_year + 1}-08-15'}
-            ]
-        }
-
         # Fetch the PDF
         response = requests.get(pdf_url)
         if response.status_code != 200:
@@ -64,10 +97,9 @@ class Command(BaseCommand):
                 model='gpt-4',
                 input='You can only output JSON. Please parse the following '
                 f'UCB academic calendar text into JSON format:\n\n{text}\n\n'
-                f'Follow this JSON format:\n\n'
-                f'{json.dumps(output_json_example, indent=2)}. We only care '
+                f'Follow this JSON format:\n\n {EXAMPLE_JSON}.\n\nWe only care'
                 f'about the semester names and dates. Do not include any other '
-                f' information.'
+                f'information.'
             )
             
             parsed_data = response.output_text
@@ -79,7 +111,7 @@ class Command(BaseCommand):
         try:
             parsed_json = json.loads(parsed_data)
             self.stdout.write(self.style.SUCCESS(
-                'Parsed JSON: {}'.format(json.dumps(parsed_json, indent=2))))
+                json.dumps(parsed_json, indent=2)))
         except json.JSONDecodeError as e:
             self.stdout.write(self.style.ERROR(
                 'Failed to parse JSON: {}'.format(e)))
